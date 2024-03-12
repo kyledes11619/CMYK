46f96dbfa6d2f475c2ebd7ace2b38cda3b8dc214
wildcardgamez <cjwilson1112@knights.ucf.edu>
2024-03-11 23:42:27 -0400

    Basic color system

---
+++ Assets/Scripts/InkSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkSystem : MonoBehaviour
+{
+    public int color = 3;
+    public Material[] colorMats;
+    public MeshRenderer[] coloredMeshes;
+
+    void Start()
+    {
+        SetColor(color);
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("DPadX") != 0)
+            SetColor(Input.GetAxis("DPadX") == 1 ? 2 : 0);
+        else if (Input.GetAxis("DPadY") != 0)
+            SetColor(Input.GetAxis("DPadY") == 1 ? 1 : 3);
+    }
+
+    void SetColor(int i)
+    {
+        color = i;
+        foreach (MeshRenderer m in coloredMeshes)
+            m.material = colorMats[color];
+    }
+}
