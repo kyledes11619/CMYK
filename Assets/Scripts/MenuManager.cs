b66c22fa8b8db95f82a06b95527969ee7b56145a
Celuan1999 <ch520385@ucf.edu>
2024-03-15 16:13:58 -0400

    Magenta Level & Menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MenuManager : MonoBehaviour
+{
+    public void ExitButton()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene( "Hub_World");
+
+    }
+
+    public void GoToMenu() {
+        SceneManager.LoadScene( "Menu");
+    }
+
+    public void Retry() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
