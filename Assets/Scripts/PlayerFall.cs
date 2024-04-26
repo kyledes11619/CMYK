cc01e5fed542800defafe0d59e8471fa5990782f
Celuan1999 <ch520385@ucf.edu>
2024-04-26 16:06:09 -0400

    fall fix

--- Assets/Scripts/PlayerFall.cs
+++ Assets/Scripts/PlayerFall.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerFall : MonoBehaviour
 {
-    public Vector3 respawn = new Vector3(803f,91f, -386f);
-
- 
+    public string WorldtoLoad;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.name == "Player")
         {
-            other.transform.position = respawn;
-            Debug.Log("Player respawned to: " + respawn);
+            SceneManager.LoadScene(WorldtoLoad);
         }
     }
 }
